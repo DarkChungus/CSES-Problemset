142e3ad9bc729b21db6f3d76b8fb60aeff31ac70
Prakrit Gajurel <96650932+DarkChungus@users.noreply.github.com>
2025-01-26 20:37:10 +0545

    Create Weird Algorithm.py

---
+++ Weird Algorithm.py
@@ -0,0 +1,13 @@
+def collatz(n):
+    print(n, end=" ")
+    while n>1:
+        if n%2 == 0:
+            n  /= 2
+        else:
+            n = 3*n + 1
+ 
+        print(int(n), end=" ")
+ 
+ 
+n = int(input(""))
+collatz(n)
